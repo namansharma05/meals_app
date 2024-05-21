eaf67ff11c5a1e9a86e762f5943750ea63885dc6
Naman <namanks0511@gmail.com>
2024-05-21 12:42:21 +0530

    project initialization and done with some of the UI

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/screens/categories.dart';
+import 'package:meals_app/screens/meals.dart';
+
+final theme = ThemeData(
+  colorScheme: ColorScheme.fromSeed(
+      seedColor: const Color.fromARGB(255, 131, 57, 0),
+      brightness: Brightness.dark),
+  textTheme: GoogleFonts.latoTextTheme(),
+);
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: theme,
+      home: const CategoriesScreen(),
+    );
+  }
+}
