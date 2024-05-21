f03248d19cec34c837dcd32b412ecb92ee578e8b
Naman <namanks0511@gmail.com>
2024-05-21 13:25:02 +0530

    added the meal items and meal item traits

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.title, required this.meals});
@@ -11,7 +12,7 @@ class MealsScreen extends StatelessWidget {
     Widget content = ListView.builder(
       itemCount: meals.length,
       itemBuilder: (context, index) {
-        return Text(meals[index].title);
+        return MealItem(meal: meals[index]);
       },
     );
     if (meals.isEmpty) {
